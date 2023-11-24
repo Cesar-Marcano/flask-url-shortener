92d4a8e69e27c318ceac6301c3fc6e551014e6c9
Cesar-Marcano <marcanocesar86@gmail.com>
2023-11-24 13:44:20 -0400

    created flask server

--- src/app.py
+++ src/app.py
@@ -1,3 +1,13 @@
+from flask import Flask
 from db import init
 
 init()
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return "hi"
+
+if __name__ == "__main__":
+    app.run(debug=True)
